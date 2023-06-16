6f59866f924a13473cfc5a95ef2dcd1c12c28c4f
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2023-06-16 15:17:07 -0300

    concluido manipulação de dados

--- models/post_equip_externo.js
+++ models/post_equip_externo.js
@@ -1,8 +1,15 @@
 const db = require('./db');
 
-const post_equip_externo = db.sequelize.define('equip_externo', {
+const tabela = "equip_externo";
+
+const post_equip_externo = db.sequelize.define(tabela, {
+    cod_registro: {
+        type: db.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     dispositivo: {
-        type: db.Sequelize.STRING
+        type: db.Sequelize.STRING,
     },
     patrimonio: {
         type: db.Sequelize.STRING
@@ -16,6 +23,9 @@ const post_equip_externo = db.sequelize.define('equip_externo', {
     obs: {
         type: db.Sequelize.TEXT
     }
+}, {
+    freezeTableName: true, //impede a pluralização automatica do nome da tabela
+    timestamps: false //Desabilita a criação automática das colunas "createdAt" e "updatedAt"
 });
 
 module.exports = post_equip_externo
